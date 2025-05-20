57a95d5b6f8b3e44cd28f65a0068f91d9c1706cb
Seung Ju Baek <seungju41625886@gmail.com>
2025-05-20 22:04:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ if user_input:
 
     # FastAPI 서버에 POST 요청
     response = requests.post(
-        "https://c206-210-97-28-7.ngrok-free.app",  # ngrok 주소 또는 포트포워딩 주소
+        "https://c206-210-97-28-7.ngrok-free.app/chat",  # ngrok 주소 또는 포트포워딩 주소
         json={"instruction": user_input, "task": selected_task}
     )
 
